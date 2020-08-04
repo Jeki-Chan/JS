0ae307c5c92b2f0dc23e4c8249e1ea9ff209cc7a
Jeki-Chan <myshka_j@ukr.net>
2020-08-04 17:00:54 +0300

    hw 1

--- Hw 1/index.js
+++ Hw 1/index.js
@@ -1,7 +1,7 @@
-alert('Введите значения:');
+alert('Для расчета обьема цилиндра введите значения:');
 
 var r = prompt('Радиус', 100);
 var h = prompt('Высота', 100);
 
 var S = Math.PI*Math.pow(r, 2);
-var V = S * h
+var V = S * h;
