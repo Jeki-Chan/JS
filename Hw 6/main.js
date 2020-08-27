20dc1f5a14534d8ba6e9ae80d074c89ec039fc04
Jeki-Chan <myshka_j@ukr.net>
2020-08-27 23:00:32 +0300

    hw 6

--- Hw 6/main.js
+++ Hw 6/main.js
@@ -1,27 +1,30 @@
-let newArr = [];
-function calculate (...theArgs) {
+function calculate (...args) {
+  let newArr = [];
 
-console.log(theArgs);
-document.write(`Переданные массивы: ${theArgs}<br><br>`);
+  console.log(args);
+  document.write(`Переданные массивы: ${args}<br><br>`);
 
-for (let i = 0; i < theArgs.length; i++) {
-  document.write(`Arr[${i}]: ${theArgs[i]}<br>`);
-  let j = theArgs[i].length % 2;
+  for (let i = 0; i < args.length; i++) {
+    document.write(`${i+1}-й массив: ${args[i]}`);
+    let j = Math.trunc(args[i].length / 2);
 
-    if (j === 0) {      
-      newArr.push(theArgs[i][j-1], theArgs[i][j]); 
-      document.write(`Центральные элементы: ${[theArgs[i][meadiana-1], theArgs[i][meadiana]]}<br>`);
-    
+    if (args[i].length === 0) {                     // !args[i].length
+      document.write(`пустой<br>`);
     } else {
-      newArr.push(theArgs[i][meadiana]);              // newArr.push([arr[i][meadiana]]);    
-      document.write(`Центральный элемент: ${theArgs[i][meadiana]}<br>`);
+      if (args[i].length % 2 === 0) {
+        newArr.push([args[i][j-1], args[i][j]]);    //  newArr.push(args[i].splice(j-1, 2)); 
+        document.write(`<br>Центральный элемент: ${[args[i][j-1], args[i][j]]}<br>`);   
+      } else {
+        newArr.push(args[i][j]);                    //  newArr.push(args[i].splice(j, 1)); 
+        document.write(`<br>Центральный элемент: ${args[i][j]}<br>`);
+      }
     }
   }
+  console.log(newArr);
   return newArr;
 }
 
-document.write(`<br>Массив из центральных элементов:  ${calculate([1, 2, 3], [1, 2], [3, 4, 5],[1],[6,5,4,3,2,1], [5,4,3,2,1])}`);
-console.log(`${newArr}`);
+document.write(`<br>Массив из центральных элементов:  ${calculate([1, 2, 3], [1, 2], [3, 4, 5], [1], [6, 5, 4, 3, 2, 1], [5, 4, 3, 2, 1], [])}`);
 document.write("<br>-----------------------------------------------------------------------------<br>");
 
 function sum (x , y) {
@@ -40,13 +43,18 @@ function factorial (x) {
   return x ? x * factorial(x - 1) : 1;
 }
 
-function doFunction(...theArgs) {  
-  if (theArgs.length === 2 && typeof theArgs[1] === "function") {
-    return theArgs[1](theArgs[0]);
-  } else if (theArgs.length === 3 && typeof theArgs[2] === "function") {
-    return theArgs[2](theArgs[0], theArgs[1]);
+function doFunction(...args) {  
+  document.write(`X: ${args[0]}, `);
+  
+  if (typeof args[args.length-1] === "function") {
+    if (args.length === 2) {
+        return args[1](args[0]);
+      } else if (args.length === 3) {
+        document.write(`Y: ${args[1]}. `);
+        return args[2](args[0], args[1]);
+      }
   } else {
-    document.write(`Eror`);
+    document.write(`Error. `);
   }
 }
 
@@ -55,5 +63,5 @@ document.write(`Div: ${doFunction(6, 3, div)}<br>`);
 document.write(`Mul: ${ doFunction(16, -23, mul)}<br>`);  
 document.write(`Power: ${doFunction(2, 3, power)}<br>`);
 document.write(`Factorial: ${doFunction(6, factorial)}<br>`);
-document.write(`: ${doFunction(2, 3, 56)}<br>`);
-document.write(`: ${doFunction(3, 56)}<br>`);
+document.write(`${doFunction(2, 3, 56)}<br>`);
+document.write(`${doFunction(3, 56)}<br>`);
