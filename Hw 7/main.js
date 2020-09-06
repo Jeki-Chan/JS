9974e1c236c91b75f0a10e2009a7df2d39b94a6b
Jeki-Chan <myshka_j@ukr.net>
2020-09-06 08:11:49 +0300

    hw 7

--- Hw 7/main.js
+++ Hw 7/main.js
@@ -3,7 +3,7 @@ let obj1 = {
   age: 30,
   "likes birds": true,
   arr0: [1, 2],
-  arr1: [],
+  arr1: ['10'],
   arr2: [3],
   arr3: [4, 'text'],
 };
@@ -13,44 +13,103 @@ let obj2 = {
   age: 31,
   surname: "Smith",
   "likes birds": false,
-  arr0: [10, 20],
-  arr1: [30, ],
-  arr2: [40, 50, 10],
+  arr: [40, 50, 10],
 };
+
+console.log(obj1);
+console.log(obj2);
 
 function doArr (object) {
   let arr = [];
-
   for (var key in object) {
-    if (object.hasOwnProperty(key) && Array.isArray(object[key])) {
+    if (Array.isArray(object[key])) {
       arr = arr.concat(object[key]);
     }
   }
-
   return arr;
 }
 
-function task1 (object1, object2) {
-  let arr = [];
-  arr = arr.concat(doArr(object1));
-  arr = arr.concat(doArr(object2));
-  return arr;
+let res = doArr(obj1).concat(doArr(obj2));
+
+document.write(`Сконкатенированные поля-массивы: ${res}<br>`);
+console.log(res);
+
+let sum = 0;
+
+for (let i = 0; i < res.length; i++) {
+  if (typeof res[i] === 'number') {
+    sum += res[i];
+  }
 }
 
-function findArrSum (arr) {
-  let sumArr = 0;
+document.write(`Сумма чисельных элементов в массивах: ${sum}`);
+document.write("<br>-----------------------------------------------------------------------------<br>");
 
-  for (let i = 0; i < arr.length; i++) {
-    if (!isNaN (+arr[i])) {
-      sumArr += arr[i];
+function inArray(str, arr) {
+  document.write(`Заданный текст: ${str}<br>`);
+  document.write(`Массив: ${arr}<br>`);
+  for (let i = 0; i < arr.length; i++) {                     //or  return arr.includes(str);
+    if (arr[i] === str) {
+      return true;
     }
   }
-  return sumArr;
+  return false;
 }
 
-const concatedArr = task1(obj1, obj2);
-const sum = findArrSum(concatedArr);
+document.write(`${inArray('hello', ['svnj', 'hello', 'cvpoq'])}<br>`);
+document.write(`${inArray('hello111', ['svnj',  'cvpoq', 'hello'])}<br>`);
+document.write("-----------------------------------------------------------------------------<br>");
 
-document.write(`Сконкатенированый массив: ${concatedArr}<br>`);
-document.write(`Сумма чисел: ${sum}<br>`);
- 
+let list = [1, 2, 3];
+document.write(`Массив: ${list}<br><br>`);
+
+const methodPush = function(...arr) {  
+  document.write(`Элементы, добавляемые в конец массива: ${arr}<br>`);
+  for (let i = 0; i < arr.length; i++) {
+    this[this.length] = arr[i];
+  }
+  return this.length;
+}
+
+Array.prototype.myPush = methodPush;
+
+document.write(`Новая длина массива: ${list.myPush(5,6)}<br>`);
+document.write(`Обновленный массив: ${list}<br>`);
+document.write("-----------------------------------------------<br>");
+
+let elements = ['Fire', 'Air', 'Water']; 
+document.write(`Массив: ${elements}<br><br>`); 
+
+const methodJoin = function(separator) {                         //or  separator = ','
+  document.write(`Разделитель: '${separator}'<br>`);
+  let str = '';
+  for (let i = 0; i < this.length; i++) {
+    if (i > 0 && i < this.length) {
+      separator === undefined ? str += ',' :  str += separator;  // str += separator === undefined ?  ',' :  separator;  
+    }
+    str += this[i];
+  }
+
+  return str;
+}
+Array.prototype.myJoin = methodJoin;
+
+document.write(`Строка: ${elements.myJoin()}<br>`);
+document.write(`Строка: ${elements.myJoin('')}<br>`);
+document.write(`Строка: ${elements.myJoin('-')}<br>`);
+document.write("-----------------------------------------------<br>");
+
+const methodReverse = function() {  
+  document.write(`Массив: ${this}<br>`);
+  for (let i = 0; i < this.length / 2; i++) {
+    let buf = this[i];
+    this[i] = this[this.length-1-i];
+    this[this.length-1-i] = buf;
+  }
+  return this;
+}
+
+Array.prototype.myReverse = methodReverse;
+
+document.write(`Перевернутый массив: ${[1,2,3,4].myReverse()}<br>`);
+document.write(`Перевернутый массив: ${[1,2,3,4,5].myReverse()}<br>`);
