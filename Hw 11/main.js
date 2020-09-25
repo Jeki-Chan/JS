6c7858802a9c8f187831d791eb3661fe90281e94
Jeki-Chan <myshka_j@ukr.net>
2020-09-25 19:04:39 +0300

    hw 11

---
+++ Hw 11/main.js
@@ -0,0 +1,34 @@
+let btn_pr = document.querySelector(".carousel-prev");
+let btn_n = document.querySelector(".carousel-next");
+let carousel = document.querySelector(".carousel");
+let slides = document.querySelectorAll(".carousel-item"); 
+
+
+carousel.addEventListener("click", function(event){
+  let k;
+  for (let i = 0; i < slides.length; i++) {
+    if(slides[i].classList.contains("active")) {
+      k = i;    
+    }
+  }
+
+  if (event.target === btn_n) {
+    slides[k].classList.remove("active");
+    k++;
+    slides[k].classList.add("active");
+    if (k === 2) {
+      btn_n.style.display = "none"; 
+    } else {
+      btn_pr.style.display = "block"; 
+    }
+  } else if (event.target === btn_pr) {
+    slides[k].classList.remove("active");
+    k--;
+    slides[k].classList.add("active");
+    if (k === 0) {
+      btn_pr.style.display = "none"; 
+    } else {
+      btn_n.style.display = "block"; 
+    }
+  }
+})
