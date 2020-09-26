a280e08377ed27708ed8d6c942daf9d749cae039
Jeki-Chan <myshka_j@ukr.net>
2020-09-26 19:11:30 +0300

    hw 8

--- Hw 8/main.js
+++ Hw 8/main.js
@@ -5,24 +5,24 @@ class Field {
     this.matrix = this.createMatrix();
   }
   createMatrix() {
-    let B = [];
+    let arr = [];
 
     for (let i = 0; i < this.height; i++) {
-      B[i] = [];
+      arr[i] = [];
       for (let j = 0; j < this.width; j++) {
-      B[i][j] = 0;
+      arr[i][j] = 0;
       }
     }
-    return B;
+    return arr;
   }
   renderField() {
     document.write(`<hr/>`);
     let table = document.createElement("table");
 
-    for(let i = 0; i < this.width; i++) {
+    for(let i = 0; i < this.height; i++) {
       let tr = document.createElement("tr");
       table.appendChild(tr);
-      for(let j = 0; j < this.height; j++) {
+      for(let j = 0; j < this.width; j++) {
         let td = document.createElement("td");
         tr.appendChild(td);
         td.innerHTML = this.matrix[i][j];
@@ -34,56 +34,74 @@ class Field {
   clearField() {        
     this.matrix = this.createMatrix();
   }
-  changeSize(newX, newY) {            // gde chyvak
-    this.width = newX;
-    this.height = newY;
-    document.write(`newX size= ${this.width} `);
-    document.write(`newY size= ${this.height}`);
+  changeSize(newWidth, newHeight) {
+    this.width = newWidth;
+    this.height = newHeight;
     this.matrix = this.createMatrix();
   }
-  addPerson(x,y) {          // [10-10] - chyvak 100.0 nizzzya
+  addPerson(x,y) {
+    if (!this.validateCoords(x, y)) {
+      throw new Error('Invalid coordinates');
+    }
     this.matrix[x][y] = 1;
   }
-  movePerson(x,y) {
+  movePerson(prevX,prevY,x,y) {
+    if (!this.validateCoords(x, y)) {
+      throw new Error('Invalid coordinates');
+    }
+    this.matrix[prevX][prevY] = 0;
     this.matrix[x][y] = 1;
+  }
+  validateCoords(x, y) {
+    return x >= 0 && x < this.height && y >= 0 && y < this.width;
   }
 }
 
 class Person {
   constructor (name, XPosition, YPosition) {
     this.name = name;
+
     this.XPosition = XPosition;
     this.YPosition = YPosition;
+    this.XnewPosition = XPosition;
+    this.YnewPosition = YPosition;
   }
   start(field) {
-    console.log(this.XPosition);
-    console.log(this.YPosition);
     field.addPerson(this.XPosition, this.YPosition);
   }
-  go(direction, step) {    //'left', 'right', 'top', 'bottom') и с шагом step
-
+  go(direction, step, field) {    
+    let prevX = this.XnewPosition;
+    let prevY = this.YnewPosition;
     if (direction === "left") {
-      this.stepGox = this.XPosition - step;
-    } else if (this.direction === "right") {
-      this.stepGox = this.XPosition + step;
-    } else if (this.direction === "top") {
-      this.stepGoy = this.YPosition - step;
-    } else if (this.direction === "bottom") {
-      this.stepGoy = this.YPosition + step;
+      this.YnewPosition = this.YnewPosition - step;
+    } else if (direction === "right") {
+      this.YnewPosition = this.YnewPosition + step;
+    } else if (direction === "top") {
+      this.XnewPosition = this.XnewPosition - step;
+    } else if (direction === "bottom") {
+      this.XnewPosition = this.XnewPosition + step;
     }
-
-    field.movePerson(this.stepGox, this.stepGoy);
+    field.movePerson(prevX, prevY, this.XnewPosition, this.YnewPosition);
   }
-  // resetPosition() {
-    
-  // }
+  resetPosition(field) {
+    field.movePerson(this.XnewPosition, this.YnewPosition, this.XPosition, this.YPosition);
+    this.XnewPosition = this.XPosition;
+    this.YnewPosition = this.YPosition;
+  }
 }
 
-let person = new Person("Jane", 5, 0);
-let field = new Field(10,10);
-
-field.renderField();
-person.start(field);
-field.renderField();
-person.go("right", 2);
-field.renderField();
+try {
+  let person = new Person("Jane", 5, 0);
+  let field = new Field(10,10);
+  
+  field.renderField();
+  person.start(field);
+  field.renderField();
+  person.go("right", 1, field);
+  person.go("bottom", 2, field);
+  field.renderField();
+  person.resetPosition(field);
+  field.renderField();
+} catch (e) {
+  alert(e.message);
+}
