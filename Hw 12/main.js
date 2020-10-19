758d79149159e0ff32ca0e6df72744d05c76b6b2
Jeki-Chan <myshka_j@ukr.net>
2020-10-19 18:45:33 +0300

    hw 12

--- Hw 12/main.js
+++ Hw 12/main.js
@@ -15,6 +15,10 @@ let arr = [
 let keys = ["id", "name", "surname", "age", "address", "skills"];
 let tbody = document.querySelector("tbody");
 let form = document.querySelector("form");
+let submit = document.querySelector(".submit");
+let create = document.querySelector(".add");
+create.addEventListener("click", addText);
+
 
 function createTable(arr){
   tbody.innerHTML = "";
@@ -28,6 +32,7 @@ function createTable(arr){
       td.innerHTML = arr[i][keys[j]];         // let text = document.createTextNode(arr[i][keys[j]]); td.appendChild(text);
     }
     let td = document.createElement("td");
+    td.classList.add("actions");
     tr.appendChild(td);
     let view = document.createElement("button");
     view.innerHTML = "View";
@@ -44,60 +49,115 @@ function createTable(arr){
   }
 }
 
-createTable(arr);
-
-function showText(id){
+function showText(pers){    //id[keys[0]]         id.id;
   form.style.display = "block";
+  submit.style.display = "none";
+
   for (let i = 0; i < keys.length; i++) {
-    form.elements[i].value = id[keys[i+1]];
-    if (i === 5) {      
-      if (id[keys[i]].includes("js")) {
+    form.elements[i].value = pers[keys[i+1]];
+    if (i === 5) {                                // need update
+      if (pers[keys[i]].includes("js")) {
         form.elements.js.checked = true;
-      } else if (!id[keys[i]].includes("js")){
+      } else if (!pers[keys[i]].includes("js")){
         form.elements.js.checked = false;
       }
-      if (id[keys[i]].includes("python")){
+      if (pers[keys[i]].includes("python")){
         form.elements.python.checked = true;
-      } else if (!id[keys[i]].includes("python")){
+      } else if (!pers[keys[i]].includes("python")){
         form.elements.python.checked = false;
       }
-      if (id[keys[i]].includes("php")){
+      if (pers[keys[i]].includes("php")){
         form.elements.php.checked = true;
-      }else if (!id[keys[i]].includes("php")){
+      }else if (!pers[keys[i]].includes("php")){
         form.elements.php.checked = false;
       }
     }
   }
 }
 
+function removePerson(pers) { 
+  form.style.display = "none";
+  submit.style.display = "none";
+ 
+  let tdActions = document.querySelectorAll(".actions");
+  let index = arr.findIndex((element) => element === pers);   //   const index = (element) => element === pers; arr.findIndex(index);
+  tdActions[index].innerHTML="Remove?";
+  
+  let yes = document.createElement("button");
+  yes.innerHTML = "Yes";
+  yes.addEventListener("click", () => removeYes(index));  
+  let no = document.createElement("button");
+  no.innerHTML = "No";
+  no.addEventListener("click", () => createTable(arr));   // display: block btn E V R
+  tdActions[index].appendChild(yes);
+  tdActions[index].appendChild(no);
+}
 
-function removePerson(id){
-  // agree or disagree
-  let n = id[keys[0]];
-  arr.splice(n-1, 1);
-  console.log(arr);
+function removeYes(index) {
+  arr.splice(index, 1); 
 
   createTable(arr);
 }
 
-function editText(id){
-  showText(id);
-  let submit = document.querySelector("input[type=submit]")
+
+function editText(pers){
+  showText(pers);
+
   submit.style.display = "block";
-  submit.addEventListener("click", addText);
-  // let data = {}
-  // for (let i = 0; i < form.elements.length; i++) {
-  //   let element = form.elements[0];
-  //   let name = element.name;
-  //   let value = element.value;
-  //   data[name] = value;
+  submit.addEventListener("click", () => updateArr(pers));
+}
+
+function updateArr(pers) {
+  for (let j = 0; j < form.elements.length; j++) {
+    let element = form.elements[j];
+    let name = element.name;
+    let value = element.value;
     
-  // }
+    if (element.type === "checkbox" &&  element.checked) {
+      console.log(value);
+      pers[name] = [];
+      pers[name].push(value);
+    } else {
+      pers[name] = value;
+    }
+  }
+
+  createTable(arr);
 }
 
-let create = document.querySelector(".add");
-create.addEventListener("click", addText);
 
-function addText(){
+
+function addText(){               // need update
   console.log("addText");
+//   let obj = {};
+
+//   console.log(form.elements);
+//   console.dir(form.elements);
+//   for (let i = 0; i < form.elements.length; i++) {
+//     let element = form.elements[i];
+//     let name = element.name;
+//     let value; //= element.value;
+    
+//     if (element.type === "checkbox" &&  element.checked) {
+// //          obj[name] = element.value;
+//       console.log(obj);   //{}
+//       console.log(name);  //skills
+//       console.log(obj[name]);  //underfined = skills {} 
+//       console.log(element.value);  //pphp.python
+//       obj[name].push(element.value);
+//     } 
+//     // if () {
+
+//     // }
+
+//   }
+  
+//   arr.push(obj);
+//   console.log(arr);
+//   console.log(obj);
+//   console.log(obj.skills);
+//   return obj;
 }
+
+
+createTable(arr);
