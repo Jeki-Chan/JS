272afaa65ba9d2d3d35bd0f281cdadf1f0c60b0a
Jeki-Chan <myshka_j@ukr.net>
2020-10-21 18:44:06 +0300

    hw 12

--- Hw 12/main.js
+++ Hw 12/main.js
@@ -1,27 +1,25 @@
-let arr = [
-  {
-    id: 1, 
-    name: "Oleg", 
-    surname: "Hi", 
-    age: 14, 
-    address: "Oslo", 
-    skills: ["js", "python"]
-  },
-  {
-    id: 2, name: "Mike", surname: "Ivanov", age: 34, address: "Miami", skills: ["php", "python"]
+let createCount = () => {     //let count = 0;   count++;
+  let i = 0;
+  return () => {
+    return i++;
   }
-];
+}
 
+let createId = createCount();
+let buffObj;
 let keys = ["id", "name", "surname", "age", "address", "skills"];
 let tbody = document.querySelector("tbody");
 let form = document.querySelector("form");
 let submit = document.querySelector(".submit");
 let create = document.querySelector(".add");
-create.addEventListener("click", addText);
+let newsubmit = document.querySelector(".newsubmit");
+submit.addEventListener("click", () => savePerson(buffObj)); 
+create.addEventListener("click", onNewClick);
+newsubmit.addEventListener("click", () => createPerson(event));
 
-
-function createTable(arr){
+function createTbody(arr){
   tbody.innerHTML = "";
+  create.style.display = "block";
 
   for (let i = 0; i < arr.length; i++) {
     let tr = document.createElement("tr");
@@ -29,17 +27,17 @@ function createTable(arr){
     for (let j = 0; j < keys.length; j++){
       let td = document.createElement("td");
       tr.appendChild(td);
-      td.innerHTML = arr[i][keys[j]];         // let text = document.createTextNode(arr[i][keys[j]]); td.appendChild(text);
+      td.innerHTML = arr[i][keys[j]];        
     }
     let td = document.createElement("td");
     td.classList.add("actions");
     tr.appendChild(td);
     let view = document.createElement("button");
     view.innerHTML = "View";
-    view.addEventListener("click", () => showText(arr[i]));   // function () { showText(arr[i].id) });
+    view.addEventListener("click", () => fillForm(arr[i]));  
     let edit = document.createElement("button");
     edit.innerHTML = "Edit";
-    edit.addEventListener("click", () => editText(arr[i]));
+    edit.addEventListener("click", () => onEditClick(arr[i]));
     let remove = document.createElement("button");
     remove.innerHTML = "Remove";
     remove.addEventListener("click", () => removePerson(arr[i]));
@@ -49,13 +47,13 @@ function createTable(arr){
   }
 }
 
-function showText(pers){    //id[keys[0]]         id.id;
+function fillForm(pers){   
   form.style.display = "block";
   submit.style.display = "none";
 
   for (let i = 0; i < keys.length; i++) {
     form.elements[i].value = pers[keys[i+1]];
-    if (i === 5) {                                // need update
+    if (i === 5) {                               
       if (pers[keys[i]].includes("js")) {
         form.elements.js.checked = true;
       } else if (!pers[keys[i]].includes("js")){
@@ -80,7 +78,7 @@ function removePerson(pers) {
   submit.style.display = "none";
  
   let tdActions = document.querySelectorAll(".actions");
-  let index = arr.findIndex((element) => element === pers);   //   const index = (element) => element === pers; arr.findIndex(index);
+  let index = arr.findIndex((element) => element === pers);           //const index = (element) => element === pers; arr.findIndex(index);
   tdActions[index].innerHTML="Remove?";
   
   let yes = document.createElement("button");
@@ -88,76 +86,84 @@ function removePerson(pers) {
   yes.addEventListener("click", () => removeYes(index));  
   let no = document.createElement("button");
   no.innerHTML = "No";
-  no.addEventListener("click", () => createTable(arr));   // display: block btn E V R
+  no.addEventListener("click", () => createTbody(arr));  
   tdActions[index].appendChild(yes);
   tdActions[index].appendChild(no);
 }
 
 function removeYes(index) {
   arr.splice(index, 1); 
-
-  createTable(arr);
+  createTbody(arr);
 }
 
-
-function editText(pers){
-  showText(pers);
+function onEditClick(pers){
+  fillForm(pers);
+  buffObj = pers;
 
   submit.style.display = "block";
-  submit.addEventListener("click", () => updateArr(pers));
+} 
+
+function savePerson(pers) {
+  event.preventDefault();
+  submit.style.display = "none";
+
+  let res = createFromForm();
+  for (let j = 1; j < keys.length; j++) {
+    pers[keys[j]] = res[keys[j]];
+  }
+
+  createTbody(arr);
 }
 
-function updateArr(pers) {
+function createFromForm() {
+  let obj = {};
+  obj.id= "";
   for (let j = 0; j < form.elements.length; j++) {
     let element = form.elements[j];
     let name = element.name;
     let value = element.value;
     
     if (element.type === "checkbox" &&  element.checked) {
-      console.log(value);
-      pers[name] = [];
-      pers[name].push(value);
+      obj[name] = [];
+      obj[name].push(value);
     } else {
-      pers[name] = value;
+      obj[name] = value;
     }
-  }
-
-  createTable(arr);
+  }  
+  return obj;
 }
 
+function onNewClick(){     
+  form.reset();           
+  form.style.display = "block";
+  create.style.display = "none";
+  submit.style.display = "none";
 
+  newsubmit.style.display = "block";
+}
 
-function addText(){               // need update
-  console.log("addText");
-//   let obj = {};
+function createPerson(event) {  
+  event.preventDefault();
 
-//   console.log(form.elements);
-//   console.dir(form.elements);
-//   for (let i = 0; i < form.elements.length; i++) {
-//     let element = form.elements[i];
-//     let name = element.name;
-//     let value; //= element.value;
+  let res = createFromForm();  
+  res.id = createId();
+  arr.push(res);
     
-//     if (element.type === "checkbox" &&  element.checked) {
-// //          obj[name] = element.value;
-//       console.log(obj);   //{}
-//       console.log(name);  //skills
-//       console.log(obj[name]);  //underfined = skills {} 
-//       console.log(element.value);  //pphp.python
-//       obj[name].push(element.value);
-//     } 
-//     // if () {
-
-//     // }
-
-//   }
-  
-//   arr.push(obj);
-//   console.log(arr);
-//   console.log(obj);
-//   console.log(obj.skills);
-//   return obj;
+  createTbody(arr);
 }
 
+let arr = [
+  {
+    id: createId(), 
+    name: "Oleg", 
+    surname: "Hi", 
+    age: 14, 
+    address: "Oslo", 
+    skills: ["js", "python"]
+  },
+  {
+    id: createId(), name: "Mike", surname: "Ivanov", age: 34, address: "Miami", skills: ["php", "python"]
+  }
+];
 
-createTable(arr);
+createTbody(arr);
