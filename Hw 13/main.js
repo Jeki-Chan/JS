749d0da34052a88a54350d51d5a3fa896b6161aa
Jeki-Chan <myshka_j@ukr.net>
2020-10-22 01:13:17 +0300

    hw 13

--- Hw 13/main.js
+++ Hw 13/main.js
@@ -5,7 +5,6 @@ let createCount = () => {     //let count = 0;   count++;
   }
 }
 
-let buffObj;
 let createId = createCount();
 let keys = ["id", "name", "surname", "age", "address", "skills"];
 let tbody = document.querySelector("tbody");
@@ -13,6 +12,7 @@ let form = document.querySelector("form");
 let submit = document.querySelector(".submit");
 let create = document.querySelector(".add");
 let newsubmit = document.querySelector(".newsubmit");
+let buffObj;
 
 submit.addEventListener("click", () => savePerson(buffObj)); 
 create.addEventListener("click", onNewClick);
@@ -33,15 +33,19 @@ function createTbody(persons){
     let td = document.createElement("td");
     td.classList.add("actions");
     tr.appendChild(td);
+
     let view = document.createElement("button");
     view.innerHTML = "View";
     view.addEventListener("click", () => fillForm(persons[i]));  
+
     let edit = document.createElement("button");
     edit.innerHTML = "Edit";
     edit.addEventListener("click", () => onEditClick(persons[i]));
+
     let remove = document.createElement("button");
     remove.innerHTML = "Remove";
     remove.addEventListener("click", () => removePerson(persons[i]));
+
     td.appendChild(edit);
     td.appendChild(view);
     td.appendChild(remove);
@@ -53,31 +57,35 @@ function fillForm(pers){
   submit.style.display = "none";
   newsubmit.style.display = "none";
 
-  for (let i = 0; i < keys.length; i++) {
-    form.elements[i].value = pers[keys[i+1]];
-    if (i === 5) {                               
-      if (pers[keys[i]].includes("js")) {
-        form.elements.js.checked = true;
-      } else if (!pers[keys[i]].includes("js")){
-        form.elements.js.checked = false;
-      }
-      if (pers[keys[i]].includes("python")){
-        form.elements.python.checked = true;
-      } else if (!pers[keys[i]].includes("python")){
-        form.elements.python.checked = false;
-      }
-      if (pers[keys[i]].includes("php")){
-        form.elements.php.checked = true;
-      }else if (!pers[keys[i]].includes("php")){
-        form.elements.php.checked = false;
-      }
+  for (let i = 0; i < keys.length - 1; i++) {
+    if (Array.isArray(pers[keys[i+1]])) {
+      form.elements.js.checked = pers[keys[i+1]].includes("js");
+      form.elements.python.checked = pers[keys[i+1]].includes("python");
+      form.elements.php.checked = pers[keys[i+1]].includes("php");
+
+      // if (pers[keys[i+1]].includes("js")) {
+      //   form.elements.js.checked = true;
+      // } else if (!pers[keys[i+1]].includes("js")){
+      //   form.elements.js.checked = false;
+      // }
+      // if (pers[keys[i+1]].includes("python")){
+      //   form.elements.python.checked = true;
+      // } else if (!pers[keys[i+1]].includes("python")){
+      //   form.elements.python.checked = false;
+      // }
+      // if (pers[keys[i+1]].includes("php")){
+      //   form.elements.php.checked = true;
+      // } else if (!pers[keys[i+1]].includes("php")){
+      //   form.elements.php.checked = false;
+      // }
+    } else {
+      form.elements[i].value = pers[keys[i+1]];
     }
   }
 }
 
 function removePerson(pers) { 
   form.style.display = "none";
-  submit.style.display = "none";
  
   let tdActions = document.querySelectorAll(".actions");
   let index = persons.findIndex((element) => element === pers);           //const index = (element) => element === pers; arr.findIndex(index);
@@ -86,15 +94,18 @@ function removePerson(pers) {
   let yes = document.createElement("button");
   yes.innerHTML = "Yes";
   yes.addEventListener("click", () => removeYes(index));  
+
   let no = document.createElement("button");
   no.innerHTML = "No";
   no.addEventListener("click", () => createTbody(persons));  
+
   tdActions[index].appendChild(yes);
   tdActions[index].appendChild(no);
 }
 
 function removeYes(index) {
   persons.splice(index, 1); 
+
   localStorage.setItem('test', JSON.stringify(persons));
   createTbody(persons);
 }
@@ -102,35 +113,37 @@ function removeYes(index) {
 function onEditClick(pers){
   fillForm(pers);
   buffObj = pers;
-  newsubmit.style.display = "none";
 
+  newsubmit.style.display = "none";
   submit.style.display = "block";
 } 
 
 function savePerson(pers) {
   event.preventDefault();
-  submit.style.display = "none";
+  form.style.display = "none";
 
   let res = createFromForm();
   for (let j = 1; j < keys.length; j++) {
     pers[keys[j]] = res[keys[j]];
   }
-  localStorage.setItem('test', JSON.stringify(persons));
 
+  localStorage.setItem('test', JSON.stringify(persons));
   createTbody(persons);
 }
 
 function createFromForm() {
   let obj = {};
   obj.id= "";
+  obj.skills = [];
   for (let j = 0; j < form.elements.length; j++) {
     let element = form.elements[j];
     let name = element.name;
     let value = element.value;
     
-    if (element.type === "checkbox" &&  element.checked) {
-      obj[name] = [];
-      obj[name].push(value);
+    if (element.type === "checkbox") {
+      if (element.checked) {
+        obj[name].push(value);
+      }
     } else {
       obj[name] = value;
     }
@@ -143,14 +156,13 @@ function onNewClick(){
   form.style.display = "block";
   create.style.display = "none";
   submit.style.display = "none";
-
   newsubmit.style.display = "block";
 }
 
-function createPerson(event) {  
+function createPerson() {  
+  event.preventDefault();
   form.style.display = "none";
 
-  event.preventDefault();
   let res = createFromForm();  
   res.id = createId();
   persons.push(res);
@@ -158,7 +170,6 @@ function createPerson(event) {
   localStorage.setItem('test', JSON.stringify(persons));  
   createTbody(persons);
 }
-
 
 let tmp = localStorage.getItem('test');
 let persons = [];
@@ -175,6 +186,7 @@ if (!tmp) {
 } else {
   persons = JSON.parse(tmp);
 }
+
 createTbody(persons);
 
 localStorage.setItem('test', JSON.stringify(persons));
